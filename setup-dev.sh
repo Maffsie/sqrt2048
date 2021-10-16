e0b6f8c2a032299a9a9d3ae6a204db1c978217fa
Maff <github@maff.im>
2021-10-16 20:41:24 +0100

    please work i don't want to build this shit locally :(

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+curl -fsSL https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - > /etc/apt/trusted.gpg.d/kitware.gpg
 apt update
-apt install -y python3-pillow gcc g++ gcc-arm-none-eabi cmake make python3 python3-pip python3-setuptools libsdl2-dev libsdl2-image-dev libsdl2-net-dev unzip
+apt install -y python3-pillow gcc g++ gcc-arm-none-eabi make python3 python3-pip python3-setuptools libsdl2-dev libsdl2-image-dev libsdl2-net-dev unzip software-properties-common lsb-release
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+apt remove --purge --auto-remove cmake
+apt clean all
+apt update
+apt install -y kitware-archive-keyring
+rm /etc/apt/trusted.gpg.d/kitware.gpg
+apt update
+apt install -y cmake
 pip3 install 32blit
